1c5cef7c1f9d469ea22389a8d5966fb576428bed
Kartik <kartik.gajendra@juspay.in>
2024-06-14 17:14:52 +0530

    add new tables

--- data/demo_app_db_dump.sql
+++ data/demo_app_db_dump.sql
@@ -14934,6 +14934,113 @@ ALTER TABLE ONLY test_cac.default_configs
 ALTER TABLE ONLY test_cac.dimensions
     ADD CONSTRAINT dimensions_function_name_fkey FOREIGN KEY (function_name) REFERENCES test_cac.functions(function_name);
 
+CREATE TABLE dev_cac.config_versions (
+    id bigint PRIMARY KEY,
+    config json NOT NULL,
+    config_hash TEXT NOT NULL,
+    tags varchar(100)[] check (array_position(tags, null) is null),
+    created_at timestamp without time zone DEFAULT CURRENT_TIMESTAMP NOT NULL
+);
+
+CREATE INDEX IF NOT EXISTS config_verions_tags_index ON dev_cac.config_versions USING gin(tags);
+CREATE INDEX IF NOT EXISTS config_versions_id_index ON dev_cac.config_versions(id);
+--
+-- Your SQL goes here
+CREATE TABLE dev_cac.type_templates (
+    type_name TEXT PRIMARY KEY,
+    type_schema JSON NOT NULL,
+    created_by TEXT NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL,
+    last_modified TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL
+);
+CREATE INDEX IF NOT EXISTS type_templates_index ON dev_cac.type_templates(type_name);
+CREATE INDEX IF NOT EXISTS type_templates_created_at_index ON dev_cac.type_templates(created_at);
+CREATE INDEX IF NOT EXISTS type_templates_last_modifed_index ON dev_cac.type_templates(last_modified);
+INSERT INTO dev_cac.type_templates(type_name, type_schema, created_by, created_at)
+VALUES (
+        'Number',
+        '{"type": "integer"}',
+        'user@superposition.io',
+        NOW()
+    ),
+    (
+        'Decimal',
+        '{"type": "number"}',
+        'user@superposition.io',
+        NOW()
+    ),
+    (
+        'Boolean',
+        '{"type": "boolean"}',
+        'user@superposition.io',
+        NOW()
+    ),
+    (
+        'Enum',
+        '{"type": "string", "enum": ["android", "ios"]}',
+        'user@superposition.io',
+        NOW()
+    ),
+    (
+        'Pattern',
+        '{"type": "string", "pattern": ".*"}',
+        'user@superposition.io',
+        NOW()
+    );
+
+CREATE TABLE test_cac.config_versions (
+    id bigint PRIMARY KEY,
+    config json NOT NULL,
+    config_hash TEXT NOT NULL,
+    tags varchar(100)[] check (array_position(tags, null) is null),
+    created_at timestamp without time zone DEFAULT CURRENT_TIMESTAMP NOT NULL
+);
+
+CREATE INDEX IF NOT EXISTS config_verions_tags_index ON test_cac.config_versions USING gin(tags);
+CREATE INDEX IF NOT EXISTS config_versions_id_index ON test_cac.config_versions(id);
+--
+-- Your SQL goes here
+CREATE TABLE test_cac.type_templates (
+    type_name TEXT PRIMARY KEY,
+    type_schema JSON NOT NULL,
+    created_by TEXT NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL,
+    last_modified TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL
+);
+CREATE INDEX IF NOT EXISTS type_templates_index ON test_cac.type_templates(type_name);
+CREATE INDEX IF NOT EXISTS type_templates_created_at_index ON test_cac.type_templates(created_at);
+CREATE INDEX IF NOT EXISTS type_templates_last_modifed_index ON test_cac.type_templates(last_modified);
+INSERT INTO test_cac.type_templates(type_name, type_schema, created_by, created_at)
+VALUES (
+        'Number',
+        '{"type": "integer"}',
+        'user@superposition.io',
+        NOW()
+    ),
+    (
+        'Decimal',
+        '{"type": "number"}',
+        'user@superposition.io',
+        NOW()
+    ),
+    (
+        'Boolean',
+        '{"type": "boolean"}',
+        'user@superposition.io',
+        NOW()
+    ),
+    (
+        'Enum',
+        '{"type": "string", "enum": ["android", "ios"]}',
+        'user@superposition.io',
+        NOW()
+    ),
+    (
+        'Pattern',
+        '{"type": "string", "pattern": ".*"}',
+        'user@superposition.io',
+        NOW()
+    );
 
 --
 -- PostgreSQL database dump complete
